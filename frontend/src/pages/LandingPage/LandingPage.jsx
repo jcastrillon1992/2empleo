bea0c6f3c2f480d459af218994368671f3cf2636
Castrillon <castrillonjames@gmail.com>
2025-09-28 22:39:26 -0500

    Landing ok

--- frontend/src/pages/LandingPage/LandingPage.jsx
+++ frontend/src/pages/LandingPage/LandingPage.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import Header from "./Components/Header";
 import Hero from './Components/Hero';
+import Features from './Components/Features';
+import Analytics from './Components/Analytics';
+import Footer from './Components/Footer';
 
 const LandingPage = () => {
   return (
     <div className='min-h-screen'>
       <Header />
       <Hero />
+      <Features />
+      <Analytics />
+      <Footer />
     </div>
   )
 }
