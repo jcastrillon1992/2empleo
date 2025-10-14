58710fa7c1415092dc51691111896e6937601737
Castrillon <castrillonjames@gmail.com>
2025-10-13 20:15:12 -0500

    modelos bd analytics application job aplication job savedjob user

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,1 +1,11 @@
+// routes/authRoutes.js
+const express = require("express");
+const router = express.Router();
+
+// Ruta temporal para evitar errores
+router.get("/", (req, res) => {
+  res.send("Auth route funcionando...");
+});
+
+module.exports = router;
 
